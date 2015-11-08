c85bf32322cd705c8017e1ab383eddd1b955cc23
Sebastian <bitterulf@users.noreply.github.com>
2015-11-08 14:33:21 +0100

    Add linting and mocha testing.

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe('Fake', function(){
+  describe('fake action', function(){
+    it('should be true', function(){
+      assert.equal(true, true);
+    })
+  })
+});
