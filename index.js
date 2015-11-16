b512d05ed7536a5aaab2a028d7ee42501dc13819
Sebastian <bitterulf@users.noreply.github.com>
2015-11-16 22:49:22 +0100

    Add json file store.

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ if (!process.argv[2] || !process.argv[3]) {
 
 var seneca = require('seneca')();
 
-require('./server.js')({city: process.argv[3], host: 'localhost', port: process.argv[2], seneca: seneca, interval: 1, test: true}, function(err) {
+seneca.use('jsonfile-store', {
+  folder:'data'
+});
+
+require('./server.js')({city: process.argv[3], host: 'localhost', port: process.argv[2], seneca: seneca, interval: 60, test: true}, function(err) {
+  setInterval(function(){
+    seneca.act({role:'trigger', cmd:'run'}, function (err, result) {
+      console.log('triggered');
+    });
+  }, 60 * 1000);
   if (err) {
     throw err;
   }
